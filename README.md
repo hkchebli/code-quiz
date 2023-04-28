4c9e3ebbf5eb840e64e86a82cdbe461b6e321b7a
hkchebli <38819813+hkchebli@users.noreply.github.com>
2023-04-27 21:42:24 -0400

    final update

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# code-quiz
+# 04 Web APIs: Code Quiz
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+When I open the page, a styled message will appear and give quiz instructions
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+## Struggles
+
+```
+I struggled with the localstorage. I had a problem using the getItem and setItem functions to get the highscores and current scores and use them in other pages.
+```
+## Resources
+https://css-tricks.com/snippets/css/typewriter-effect/
+https://www.youtube.com/watch?v=DFhmNLKwwGw
+
+## Notes
+
+```
+I added comments to every major line of code to keep it clear and understandable
+Researched concepts online using different sources such as MDN 
+I
+```
+
+## Screenshot Demonstration
+in this case the user chose only special characters and uppercase letters with a password length of 48 
+
+<img src="page1.png"/>
+<img src="page2.png"/>
+<img src="page3.png"/>
+
+## Deployed link
+```
+please find the deployed link below:
+```
+https://hkchebli.github.io/code-quiz
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+//initialize variables 
 var wins = 0;
-
 var student = document.getElementById("student-names");
 var grade = document.getElementById("grades");
 var comment = document.getElementById("msg");
@@ -14,6 +14,9 @@ var corrAnswer = 0;
 var wrongAnswer = 0;
 var correct;
 
+scoreCount=0;
+var displayScore=document.getElementById("score");
+
 var option1 = document.getElementById("option1");
 var option2 = document.getElementById("option2");
 var option3 = document.getElementById("option3");
@@ -21,6 +24,65 @@ var option4 = document.getElementById("option4");
 //var options =document.querySelectorAll("options");
 var optionss = document.getElementById("options");
 
+//used to style the quiz intructions
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 1) || 300;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+//function to render questions
 var renderQuestion = function (questionNum) {
     console.log("render question: " + questionNum);
     option1.textContent = Questions[questionNum].choice1;
@@ -35,6 +97,7 @@ var renderQuestion = function (questionNum) {
     correct = Questions[questionNum].Answer;
 }
 
+//object that stores all set of questions and options and correct answer to each question
 var Questions = [{
     question: "How many elements can you apply an 'ID' attribute to?",
     choice1: "As many as you want",
@@ -58,14 +121,32 @@ var Questions = [{
     choice3: "Python",
     choice4: "React.js",
     Answer: "CSS"
-}
+},
+    {
+    question: "When is localStorage data cleared?",
+    choice1: "No expiration time",
+    choice2: "On page reload",
+    choice3: "On browser close",
+    choice4: "On computer restart",
+    Answer: "No expiration time"
+},
+{
+    question: "What does WWW stand for?",
+    choice1: "Web World Workings",
+    choice2: "Weak Winter Wind",
+    choice3: "World Wide Web",
+    choice4: "Wendy Wants Waffles",
+    Answer: "World Wide Web"
+},
 ];
 
+//event listener to start game when click start button
 startButton.addEventListener("click", function (event) {
 
-
+   
     console.log(event);
     let losscount = 0;
+    // timer 
     let timeRemaining = 60;
     const timer = setInterval(() => {
         if (timeRemaining > 0) {
@@ -83,42 +164,45 @@ startButton.addEventListener("click", function (event) {
 
     }, 1000);
 
-
+    // render first question
     renderQuestion(0);
-    
-    var i = 1;
 
+    var i = 1;
+    //event listener for when user user chooses option
     optionss.addEventListener("click", function (event) {
-        
+        //sends user input and checks if answer is correct
         checkAnswer(event.target.value);
 
         console.log(event.target.value);
 
-        
-        
-        console.log("the value of i is: "+ i)
+
+
+        console.log("the value of i is: " + i)
+        //checks if all questions have been asked
         if (i === (Questions.length)) {
-            
             endGame();
         }
-        else{
+        else {
             renderQuestion(i);
         }
         i++;
 
     });
-
+    //function to check if chosen option is corrent answer or not
     function checkAnswer(answer) {
 
 
         if (answer === correct) {
             corrAnswer++;
+            scoreCount=scoreCount+10;
+            displayScore.textContent=scoreCount;
             display_corrAnswer.textContent = corrAnswer;
             console.log(" this is correct count: " + corrAnswer);
 
         } else {
 
             wrongAnswer++;
+            timeRemaining = timeRemaining - 10;
             display_wrongAnswer.textContent = wrongAnswer;
             console.log(" this is worng count: " + wrongAnswer);
 
@@ -128,10 +212,16 @@ startButton.addEventListener("click", function (event) {
 
     }
 
+   //function ends the game and sends user to endgame page
     function endGame() {
-        console.log("GAME OVER!");
+        
+       
+        return window.location.assign("./endgame.html");
     }
+    
+
 });
+
 
 
 
